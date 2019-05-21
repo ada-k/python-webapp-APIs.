5c659fe11a75d90045e162b97de82c36a9198ee7
Adah K <adakibetj@protonmail.com>
2019-05-21 09:22:32 +0300

    Update post.py

--- models/post.py
+++ models/post.py
@@ -1,45 +1,45 @@
-#
-# import uuid
-# #from datetime
-# import datetime
-#
-# from src.common.database import Database
-#
-# '''from src.common.pymongo.database import Database'''
-#
-#
-#
-#
-# class Post(object):
-#     def __init__(self, blog_id, title, content, author, created_date=datetime.datetime.utcnow(), _id=None):
-#         self.blog_id = blog_id
-#         self.title = title
-#         self.content =content
-#         self.author =author
-#         self.created_date =created_date
-#         self._id = uuid.uuid4().hex if _id is None else _id
-#
-#     def save_to_mongo(self):
-#         Database.insert(collection='posts', data=self.json())
-#
-#
-#
-#     def json(self):
-#         return {'blog_id' :self.blog_id,
-#          'title':self.title,
-#          'content':self.content,
-#          'author':self.author,
-#          'date':self.created_date,
-#          '_id':self._id
-#         }
-#
-#     @classmethod
-#     def from_mongo(cls, id):
-#         post_data= Database.find_one(collection='posts', query={'_id': id})
-#         return cls(**post_data)
-#
-#     @staticmethod
-#     def from_blog(id):
-#         return [post for post in Database.find(collection='posts', query={'blog_id': id})]
-#
-#
+
+import uuid
+#from datetime
+import datetime
+
+from src.common.database import Database
+
+'''from src.common.pymongo.database import Database'''
+
+
+
+
+class Post(object):
+    def __init__(self, blog_id, title, content, author, created_date=datetime.datetime.utcnow(), _id=None):
+        self.blog_id = blog_id
+        self.title = title
+        self.content =content
+        self.author =author
+        self.created_date =created_date
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    def save_to_mongo(self):
+        Database.insert(collection='posts', data=self.json())
+
+
+
+    def json(self):
+        return {'blog_id' :self.blog_id,
+         'title':self.title,
+         'content':self.content,
+         'author':self.author,
+         'date':self.created_date,
+         '_id':self._id
+        }
+
+    @classmethod
+    def from_mongo(cls, id):
+        post_data= Database.find_one(collection='posts', query={'_id': id})
+        return cls(**post_data)
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in Database.find(collection='posts', query={'blog_id': id})]
+
+
